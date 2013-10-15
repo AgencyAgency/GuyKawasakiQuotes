9ab8b9622e244210a31cf8b089f2f7c62f87a9ca
caseymorrissey <cmorrissey@US-S414LAB-LAP10.local>
2013-10-15 07:24:17 -1000

    Added quote
    "What I lack is talent, I compensate by willing to grind it out. That's
    the secret of my life."

--- AAGuyKawasakiQuotes/AAViewController.m
+++ AAGuyKawasakiQuotes/AAViewController.m
@@ -64,6 +64,9 @@
                     @"Pursuing your passions makes you more interesting, and interesting people are enchanting.",
                     @"Lots of things made the first Mac in 1984 a piece of crap, but it was a revolutionary piece of crap.",
                     @"Inertia. Guy’s law of enchantment: “People at rest will remain at rest, and people in motion will keep moving in the same direction unless an outside enchanter acts upon them."
+                    @"What I lack is talent, I compensate by willing to grind it out. That's the secret of my life."
+
+
                     ];
     }
     return _quotes;
